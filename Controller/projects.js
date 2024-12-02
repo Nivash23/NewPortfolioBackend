675d681bb4a34843560b2fd7f533055411eef3d4
Nivash23 <nivashsubash32@gmail.com>
2024-12-02 14:18:01 +0530

    projectupdate.

--- Controller/projects.js
+++ Controller/projects.js
@@ -3,10 +3,10 @@ const ProjectRouter = require('express').Router();
 const projects = require('../model/projects');
 
 ProjectRouter.get('/', async (req, res) => {
-    const Allprojects = await projects.find();
+    const allprojects = await projects.find();
 
     res.status(200).json({
-        Allprojects
+        Allprojects:allprojects,
     })
 })
 
